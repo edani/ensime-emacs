fd669dbfb435b641f391e300c1861a248d056f0b
Aemon Cannon <aemoncannon@gmail.com>
2012-05-23 00:52:29 -0400

    Fix some elisp errors visible in compilation

--- src/main/elisp/ensime-debug.el
+++ src/main/elisp/ensime-debug.el
@@ -19,6 +19,8 @@
 ;;     Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
 ;;     MA 02111-1307, USA.
 
+(eval-and-compile
+  (require 'cl))
 
 (defgroup ensime-db nil
   "Customization of ensime debugger support."
