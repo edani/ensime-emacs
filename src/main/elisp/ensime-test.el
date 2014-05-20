af62bc654a2f754add46aeda5c87c711aa787ead
Eric Daniel <eric-github@barberic.org>
2014-05-19 22:23:55 -0700

    Make "inspect type at range" test work in Windows

--- src/main/elisp/ensime-test.el
+++ src/main/elisp/ensime-test.el
@@ -664,14 +664,12 @@
       (ensime-test-eat-label "1")
       (ensime-typecheck-current-file)
       (forward-char 1)
-      (let ((mark (point)))
-        (goto-char (point-at-eol))
-        (let* ((info (ensime-rpc-inspect-type-at-range
-                      (list (- mark 1) (- (point) 1)))))
-          (ensime-assert (not (null info)))
-          (ensime-assert-equal
-           (plist-get (plist-get info :type) :name) "String")
-          ))
+      (let ((info (ensime-rpc-inspect-type-at-range
+                   (list (ensime-externalize-offset (point))
+                         (ensime-externalize-offset (point-at-eol))))))
+        (ensime-assert (not (null info)))
+        (ensime-assert-equal
+	 (plist-get (plist-get info :type) :name) "String"))
       (ensime-test-cleanup proj)))
     )
 
