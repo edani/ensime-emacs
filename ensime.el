da7749d1f91cb769e66d01661b8c0c59412d66ef
Sam Halliday <sam.halliday@gmail.com>
2014-06-29 12:37:39 +0100

    default server version is "2.10" because there is no minor release number

--- ensime.el
+++ ensime.el
@@ -169,9 +169,9 @@
   :group 'ensime-server)
 
 (defcustom ensime-default-scala-version
-  "2.10.4"
+  "2.10"
   "Default version of scala. An appropriate version of the server
-   will be obtained. Must be exact to minor release."
+   will be obtained. May need to be exact to minor release."
   :type 'string
   :group 'ensime-server)
 
