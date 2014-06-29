14027551d3c67e2a3b336f63158c5258e85640a4
Eric Daniel <eric-github@barberic.org>
2014-06-29 16:35:22 -0700

    2nd try at splitting up ensime.el

--- ensime-builder.el
+++ ensime-builder.el
@@ -19,8 +19,7 @@
 ;;     Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
 ;;     MA 02111-1307, USA.
 
-(require 'ensime-core)
-(require 'ensime-connections)
+(require 'ensime-client)
 
 (defun ensime-builder-build ()
   "Start the incremental builder. This command will trigger
