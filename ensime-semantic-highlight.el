23c8ebf93be21fa5997ce2612dba9e87e5707eb7
Eric Daniel <eric-github@barberic.org>
2014-07-12 23:40:44 -0700

    Derive more ensime faces from standard faces.
    Enable semantic highlighting by default. Fix ensime/ensime-server#490

--- ensime-semantic-highlight.el
+++ ensime-semantic-highlight.el
@@ -20,42 +20,6 @@
 ;;     MA 02111-1307, USA.
 
 
-(defvar ensime-sem-high-all-faces
-  '(
-    (var . (:foreground "#ff2222"))
-    (val . (:foreground "#dddddd"))
-    (varField . (:foreground "#ff3333"))
-    (valField . (:foreground "#dddddd"))
-    (functionCall . (:foreground "#84BEE3"))
-    (operator . (:foreground "#bbbbbb"))
-    (param . (:foreground "#ffffff"))
-    (class . font-lock-type-face)
-    (trait . (:foreground "#084EA8"))
-    (object . (:foreground "#026DF7"))
-    (package . font-lock-preprocessor-face)
-    ))
-
-(defvar ensime-sem-high-default-faces
-  '())
-
-(defvar ensime-sem-high-faces
-  ensime-sem-high-default-faces
-  "Faces for semantic highlighting. Symbol types not mentioned here
- will not be requested from server.")
-
-(defun ensime-sem-high-enable-all ()
-  "Enable full semantic highlighting."
-  (interactive)
-  (setq ensime-sem-high-faces
-	ensime-sem-high-all-faces)
-  (ensime-sem-high-refresh-all-buffers))
-
-(defun ensime-sem-high-disable-all ()
-  "Disable all semantic highlighting."
-  (interactive)
-  (setq ensime-sem-high-faces '())
-  (ensime-sem-high-refresh-all-buffers))
-
 (defun ensime-sem-high-apply-properties (info)
   "Use provided info to modify font-lock properties of identifiers
  in the program text."
@@ -110,14 +74,15 @@
 
 (defun ensime-sem-high-refresh-region (beg end)
   "Refresh semantic highlighting for the given region."
-  (ensime-rpc-symbol-designations
-   buffer-file-name beg end
-   (mapcar 'car ensime-sem-high-faces)
-   `(lambda (info)
-      (ensime-sem-high-clear-region ,beg ,end)
-      (ensime-sem-high-apply-properties info)
-      (ensime-event-sig :region-sem-highlighted nil)
-      )))
+  (when ensime-sem-high-enabled-p
+    (ensime-rpc-symbol-designations
+     buffer-file-name (ensime-externalize-offset beg) (ensime-externalize-offset end)
+     (mapcar 'car ensime-sem-high-faces)
+     `(lambda (info)
+        (ensime-sem-high-clear-region ,beg ,end)
+        (ensime-sem-high-apply-properties info)
+        (ensime-event-sig :region-sem-highlighted nil)
+        ))))
 
 (defun ensime-sem-high-inspect-highlight ()
   (interactive)
