98769943e8c8338e11a8978f50a734eeee2de24c
Eric Daniel <eric-github@barberic.org>
2014-10-31 23:24:41 -0700

    Fix ensime/ensime-server#424
    Pulled changes from @slipset ; added menu entries ; made stacktrace
    highlight to work.

--- ensime-pkg.el
+++ ensime-pkg.el
@@ -1,6 +1,7 @@
 (define-package "ensime" "0.9.10"
   "ENhanced Scala Interaction Mode for Emacs"
-  '((s "1.3.0") (dash "2.8.0") (popup "0.5.0") (auto-complete "1.4.0") (scala-mode2 "0.21")))
+  '((s "1.3.0") (dash "2.8.0") (popup "0.5.0") (auto-complete "1.4.0") (scala-mode2 "0.21")
+    (sbt "20141028.1023")))
 
 ;; Local Variables:
 ;; no-byte-compile: t
