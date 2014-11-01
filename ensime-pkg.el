eb1230368c9dc387207bff2fe28c5ad1bde3aaa7
Sam Halliday <sam.halliday@Gmail.com>
2014-11-01 16:09:33 +0000

    hotfix https://github.com/ensime/ensime-server/issues/685

--- ensime-pkg.el
+++ ensime-pkg.el
@@ -1,7 +1,11 @@
 (define-package "ensime" "0.9.10"
   "ENhanced Scala Interaction Mode for Emacs"
-  '((s "1.3.0") (dash "2.8.0") (popup "0.5.0") (auto-complete "1.4.0") (scala-mode2 "0.21")
-    (sbt "20141028.1023")))
+  '((s "1.3.0")
+    (dash "2.8.0")
+    (popup "0.5.0")
+    (auto-complete "1.4.0")
+    (scala-mode2 "0.21")
+    (sbt "0.03")))
 
 ;; Local Variables:
 ;; no-byte-compile: t
