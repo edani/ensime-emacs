6fd7e511c2f3efb933da825c6b36343c562f312b
Aemon Cannon <aemoncannon@gmail.com>
2014-12-18 13:27:58 -0500

    Add company-mode backend - Includes support for async candidate retrieval - Function calls now expanded as yasnippet templates - Configured as the new default using ensime-completion-style
    Add an Emacs 24 CAP backend.
    
    Two new dependencies:
    - yasnippet
    - company-mode

--- ensime-pkg.el
+++ ensime-pkg.el
@@ -5,7 +5,10 @@
     (popup "0.5.0")
     (auto-complete "1.4.0")
     (scala-mode2 "0.21")
-    (sbt-mode "0.03")))
+    (sbt-mode "0.03")
+    (company "0.8.7")
+    (yasnippet "0.8.0")
+    ))
 
 ;; Local Variables:
 ;; no-byte-compile: t
