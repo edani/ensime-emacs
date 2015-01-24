6143d3ea216eb4510c48a74d240e00a24436a72c
Aemon Cannon <aemoncannon@gmail.com>
2015-01-23 21:29:05 -0600

    Install the packages from ensime-pkg.el for testing.

--- ensime-pkg.el
+++ ensime-pkg.el
@@ -1,7 +1,7 @@
 (define-package "ensime" "0.9.10"
   "ENhanced Scala Interaction Mode for Emacs"
   '((s "1.3.0")
-    (dash "2.8.0")
+    (dash "2.10.0")
     (popup "0.5.0")
     (auto-complete "1.4.0")
     (scala-mode2 "0.21")
