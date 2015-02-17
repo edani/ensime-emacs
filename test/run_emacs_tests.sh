d46244ca29d201ed38965759fbf40492eea22166
Sam Halliday <sam.halliday@gmail.com>
2015-02-17 21:22:10 +0000

    bump scala test version (to match publishLocal version)

--- test/run_emacs_tests.sh
+++ test/run_emacs_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export ENSIME_RUN_AND_EXIT=1
-export ENSIME_TEST_SERVER_VERSION=2.11.4
+export ENSIME_TEST_SERVER_VERSION=2.11.5
 
 if [ $# -ge 1 ]; then
   emacs --no-init-file --load ./dotemacs_test.el --eval '(ensime-run-one-test "'"$*"'")'
