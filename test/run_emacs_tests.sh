5c6e5e9205902bf012180ead76bcead1122df3cc
Sam Halliday <sam.halliday@gmail.com>
2015-02-27 22:26:29 +0000

    trivial: fix running tests locally

--- test/run_emacs_tests.sh
+++ test/run_emacs_tests.sh
@@ -4,7 +4,7 @@ export ENSIME_RUN_AND_EXIT=1
 export ENSIME_TEST_SERVER_VERSION=2.11.5
 
 if [ $# -ge 1 ]; then
-  emacs --no-init-file --load ./dotemacs_test.el --eval '(ensime-run-one-test "'"$*"'")'
+  emacs --no-init-file --load test/dotemacs_test.el --eval '(ensime-run-one-test "'"$*"'")'
 else
-  emacs --no-init-file --load ./dotemacs_test.el  --eval '(ensime-run-all-tests)'
+  emacs --no-init-file --load test/dotemacs_test.el  --eval '(ensime-run-all-tests)'
 fi
