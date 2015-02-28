9a794eb876ae1fe14902a1f9acbea633b89813c2
Sam Halliday <sam.halliday@gmail.com>
2015-02-28 16:18:01 +0000

    no need for a custom test runner in travis

--- test/run_emacs_tests.sh
+++ test/run_emacs_tests.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
 
-export ENSIME_RUN_AND_EXIT=1
-export ENSIME_TEST_SERVER_VERSION=2.11.5
+if [ -z "$ENSIME_RUN_AND_EXIT" ] ; then
+    export ENSIME_RUN_AND_EXIT=1
+fi
+
+if [ -z "$ENSIME_TEST_SERVER_VERSION" ] ; then
+    export ENSIME_TEST_SERVER_VERSION=2.11.5
+fi
+
+if [ -z "$EMACS" ] ; then
+    export EMACS=emacs
+fi
+
+if [ "$TRAVIS" = "true" ] ; then
+    echo "Starting Xvfb..."
+    export DISPLAY=:99
+    Xvfb $DISPLAY -screen 0 1024x768x16 &
+    sleep 5
+
+    echo "Testing with emacs=$EMACS"
+    $EMACS --version
+fi
 
 if [ $# -ge 1 ]; then
-  emacs --no-init-file --load test/dotemacs_test.el --eval '(ensime-run-one-test "'"$*"'")'
+  $EMACS --no-init-file --load test/dotemacs_test.el --eval '(ensime-run-one-test "'"$*"'")'
 else
-  emacs --no-init-file --load test/dotemacs_test.el  --eval '(ensime-run-all-tests)'
+  $EMACS --no-init-file --load test/dotemacs_test.el  --eval '(ensime-run-all-tests)'
 fi
