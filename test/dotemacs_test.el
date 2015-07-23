d99f3a26909eee4a43b8519c9cbecd2faaccf0f7
Sam Halliday <sam.halliday@gmail.com>
2015-07-23 11:06:43 +0100

    clean up test runner

--- test/dotemacs_test.el
+++ test/dotemacs_test.el
@@ -10,44 +10,38 @@
           (lambda() (message "Exiting ENSIME tests at %s" (backtrace))))
 
 (require 'package)
-(require 'cl)
-
 (package-initialize)
+(when (not package-archive-contents)
+  (package-refresh-contents))
 
-(defun pkg-installed-p (pkg)
-  (package-installed-p (car pkg) (version-to-list (cadr pkg))))
+(defun ensime--install-dependency (dependency)
+  "PKG-INFO: List with package as the head and version as the tail."
+  (let ((pkg (car dependency))
+        (version (cadr dependency)))
+    (if (package-installed-p pkg (version-to-list version))
+        (message "%s is already installed" pkg)
+      (package-install pkg))))
 
 ;; Load all package dependencies
-(condition-case err
-    (let* ((pkg-info
-	    (with-temp-buffer
-	      (insert-file-contents "ensime-pkg.el")
-	      (goto-char (point-min))
-	      (read (current-buffer))))
-	   (name (cadr pkg-info))
-	   (needed-packages (cadr (nth 4 pkg-info))))
-      (assert (equal name "ensime"))
-      (message "Loaded ensime-pkg.el")
-      (message "Installing dependencies: %S" needed-packages)
-      (if (every #'pkg-installed-p needed-packages)
-	  (message "All dependencies present.")
-	(package-refresh-contents)
-	(dolist (p needed-packages)
-	  (unless (pkg-installed-p p)
-	    (package-install (car p))
-	    (when (not (pkg-installed-p p))
-	      (error (message "Failed to install %s." p)))))))
-  (error (message "Error loading dependencies: %s" err)))
+(let* ((pkg-info
+        (with-temp-buffer
+          (insert-file-contents "ensime-pkg.el")
+          (goto-char (point-min))
+          (read (current-buffer))))
+       (name (cadr pkg-info))
+       (needed-packages (cadr (nth 4 pkg-info))))
+  (message "Installing dependencies: %S" needed-packages)
+  (dolist (dependency needed-packages nil)
+    (ensime--install-dependency dependency)))
 
 ;; enable coverage
 (when (getenv "UNDERCOVER")
-  (unless (package-installed-p 'undercover)
-    (package-install 'undercover))
-  (when (require 'undercover nil t)
-    (undercover "ensime*.el"
-                (:report-file "coveralls.json")
-                (:send-report nil)
-                (:exclude "ensime-test.el" "dotemacs_test.el" "ensime-inspector.el"))))
+  (ensime--install-dependency '(undercover "0.5.0"))
+  (require 'undercover)
+  (undercover "ensime*.el"
+              (:report-file (expand-file-name "./coveralls.json"))
+              (:send-report nil)
+              (:exclude "ensime-test.el" "dotemacs_test.el" "ensime-inspector.el")))
 
 (add-to-list 'load-path (expand-file-name "./"))
 (require 'ensime)
